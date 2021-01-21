6fb087a2c68d75b7ebb1c467496468e21716eec2
arthyvelli <48116111+arthyvelli@users.noreply.github.com>
2021-01-21 15:47:14 +0530

    Update CampaignDuplicatePage.java
    added

--- src/test/java/pages/CampaignDuplicatePage.java
+++ src/test/java/pages/CampaignDuplicatePage.java
@@ -1,4 +1,5 @@
 package pages;
+import java.util.Calendar;
 import java.util.List;
 
 import org.openqa.selenium.By;
@@ -24,7 +25,7 @@ import org.openqa.selenium.support.ui.WebDriverWait;
 import com.relevantcodes.extentreports.LogStatus;
 public class CampaignDuplicatePage  {
 	private WebDriver driver;
-	
+
 	@FindBy(xpath="//*[@id='app']/div/section/div[2]/div[1]/div/div[1]/form/div[4]/div[1]/div/div[2]")
 	private WebElement SenderId;   // driver.findElement(By.xpath())
 	
@@ -45,15 +46,13 @@ public class CampaignDuplicatePage  {
 		//Select duplicate option
 		BasePage.waitForElement(driver,CampaignElements.Dup,50);
 		BasePage.ClickElement(driver,CampaignElements.Dup);
-
 		//Confimr Duplicate
-		//BasePage.performScrollToElement(driver, CampaignElements.DupConfirm1);
 		BasePage.waitForElement(driver,CampaignElements.DupConfirm1,50);
-		
+
 		BasePage.ClickElement(driver,CampaignElements.DupConfirm1);
 		//BasePage.performScrollToElement(driver, CampaignElements.Dupconfirm2);
 		BasePage.waitForElement(driver,CampaignElements.Dupconfirm2,50);
-		
+
 		BasePage.ClickElement(driver,CampaignElements.Dupconfirm2);
 
 		Thread.sleep(2000);
@@ -65,43 +64,29 @@ public class CampaignDuplicatePage  {
 		BasePage.ClickElement(driver,CampaignElements.EditDup);
 		//Select sender id 
 		BasePage.performScrollToElement(driver,SenderId);
-		System.out.println("step1");
+
 		Thread.sleep(2000);	
-				
-		System.out.println("step3");
-		
 		WebElement sendidselct = driver.findElement(By.xpath("//*[@id='app']/div/section/div[2]/div[1]/div/div[1]/form/div[4]/div[1]/div/div[2]"));
 		Actions builder = new Actions(driver);
-		
-		Action seriesOfActions = builder.moveToElement(sendidselct).sendKeys(sendidselct,Senderid).
-				sendKeys(Keys.ENTER).build();
-		
-		seriesOfActions.perform();
-		
-		
-	
-		Thread.sleep(1000);
-		
+		System.out.println("Sender Id"+Senderid);
+		builder.moveToElement(sendidselct).sendKeys(sendidselct,Senderid).
+		sendKeys(Keys.ENTER).build().perform();
+		Thread.sleep(5000);
 		//Select Choose contact form wizard
 		BasePage.waitForElement(driver,CampaignElements.ChooseContactstage,30);
 		BasePage.ClickElement(driver,CampaignElements.ChooseContactstage);
-
 		//Select list	
 		BasePage.waitForElement(driver,CampaignElements.SelectList,30);
 		BasePage.ClickElement(driver,CampaignElements.SelectList);
-
 		//Save contacts
 		BasePage.waitForElement(driver,CampaignElements.Continuebtn,30);
 		BasePage.ClickElement(driver,CampaignElements.Continuebtn);
-
 		//schedule 
 		BasePage.waitForElement(driver,CampaignElements.schedule,50);
 		BasePage.ClickElement(driver,CampaignElements.schedule);
+		System.out.println("Campaign Scheduled Successfully");
 
 	}
-
-
-
 
 	public void Schedule(String time) throws InterruptedException {
 
@@ -112,10 +97,7 @@ public class CampaignDuplicatePage  {
 		BasePage.ClickElement(driver,CampaignElements.schedTime);
 		Thread.sleep(1000);
 		BasePage.EnterText(driver,CampaignElements.schedTime,time);
-
 	}
-
-
 	public void Scheduleconfirm() throws InterruptedException  {
 		try {
 			BasePage.waitForElement(driver,CampaignElements.schedconfirm,50);
@@ -138,17 +120,12 @@ public class CampaignDuplicatePage  {
 			BasePage.waitForElement(driver, CampaignElements.camaignpreviewclose, 25);
 			BasePage.ClickElement(driver, CampaignElements.camaignpreviewclose);
 			System.out.println("trying to close the camp windoe");
-
-
 		}
 
-		/*  finally {
-    		  Thread.sleep(1000);
-    		  BasePage.waitForElement(driver,CampaignElements.closeSuccessmessage,50);
-    		  Thread.sleep(1000);
-    		  BasePage.ClickElement(driver,CampaignElements.closeSuccessmessage);
-    	  }*/
+
 	}
+
+
 }
 
 
